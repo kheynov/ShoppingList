e978dd5c0ebc5667e4ae300fc132e707b2977a79
kheynov <lcastory86@gmail.com>
2021-10-10 15:28:47 +0700

    Added some vector assets, shopping list layout, adapter for recycler view

---
+++ app/src/main/java/ru/kheynov/mvvmshoppinglist/adapters/ShoppingItemAdapter.kt
@@ -0,0 +1,50 @@
+package ru.kheynov.mvvmshoppinglist.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.shopping_item.view.*
+import ru.kheynov.mvvmshoppinglist.R
+import ru.kheynov.mvvmshoppinglist.data.db.entities.ShoppingItem
+import ru.kheynov.mvvmshoppinglist.ui.shoppinglist.ShoppingViewModel
+
+class ShoppingItemAdapter(
+    var items: List<ShoppingItem>,
+    private val viewModel: ShoppingViewModel
+) : RecyclerView.Adapter<ShoppingItemAdapter.ShoppingViewHolder>() {
+    inner class ShoppingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoppingViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.shopping_item, parent, false)
+        return ShoppingViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShoppingViewHolder, position: Int) {
+        val currentShoppingItem = items[position]
+
+        holder.itemView.tvName.text = currentShoppingItem.name
+        holder.itemView.tvAmount.text = "${currentShoppingItem.amount}"
+        holder.itemView.ivDelete.setOnClickListener {
+            viewModel.delete(currentShoppingItem)
+        }
+        holder.itemView.ivPlus.setOnClickListener {
+            currentShoppingItem.amount++
+            viewModel.upsert(currentShoppingItem)
+        }
+        holder.itemView.ivMinus.setOnClickListener {
+            if (currentShoppingItem.amount > 0){
+                currentShoppingItem.amount--
+                viewModel.upsert(currentShoppingItem)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+}
